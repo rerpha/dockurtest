d8231cfd2d35b85ed0f378b7912ff0b4ec87cee6
Jack Harper <jack.harper@stfc.ac.uk>
2026-06-17 08:10:47 +0100

    try powershell script

--- example/install.bat
+++ example/install.bat
@@ -1,3 +1,1 @@
-powershell -command "Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0"
-powershell -command "Start-Service sshd"
-powershell -command "Set-Service -Name sshd -StartupType 'Automatic'"
+powershell -executionpolicy bypass -File .\install.ps1
